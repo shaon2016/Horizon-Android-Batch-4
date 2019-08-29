25ecf3eee9d196d56cb46ff96de9e1886bf8878b
Shaon <shaondeveloper@gmail.com>
2019-08-29 20:49:12 +0600

    How to go from one class to another

--- app/src/main/java/com/example/androidbatch4day7/MainActivity.kt
+++ app/src/main/java/com/example/androidbatch4day7/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.androidbatch4day7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnGoToNext.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
